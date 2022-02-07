4af62a8b85559038a065ee9588108224ecfb5438
William z <98198668+Wilaz@users.noreply.github.com>
2022-02-07 11:51:17 -0500

    finshed readme

--- willtech_arduino_binary_to_decimal.ino
+++ willtech_arduino_binary_to_decimal.ino
@@ -1,9 +1,19 @@
 /*
- *  Willtech: arduino binary to decimal
- *  
- *  Author: William Zanin
- *  Contact: wbzanin@outlook.com
- *  All Right Reserved
+ *  Arduino - binary to decimal
+ *  Copyright (C) 2022  William Zanin
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *  
  *  credits for binary to decimal: Bansal , Sunidhi. Program for Binary to Decimal Conversion in C++, https://www.tutorialspoint.com/program-for-binary-to-decimal-conversion-in-cplusplus. 
  */
